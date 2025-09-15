18707f0213d67a12734c1a26d38f97fed1f4d451
Toft <toft.dah@gmail.com>
2025-09-15 11:17:27 +0300

    g1

---
+++ g1/MonthlyPeriod/MonthlyPeriod.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class MonthlyPeriod {
+    public String calculatePeriod(String startDate, String endDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        LocalDate start = LocalDate.parse(startDate, formatter);
+        LocalDate end = LocalDate.parse(endDate, formatter);
+
+        int months = Period.between(start, end).getMonths();
+        int years = Period.between(start, end).getYears();
+
+        if (years == 0) {
+            return "Period: " + months + "months";
+        } else {
+            return "Period: " + years + "years and " + months + "months";
+        }
+    }
+}
